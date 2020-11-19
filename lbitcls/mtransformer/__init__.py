8e04c854a46ab1400bd56a18a5666a0aba8c2411
tanfeiyang <tanfeiyang@megvii.com>
2020-11-19 21:10:15 +0800

    [tanfeiyang/master] support  transformerv2

--- lbitcls/mtransformer/__init__.py
+++ lbitcls/mtransformer/__init__.py
@@ -3,6 +3,7 @@ from .LSQ import *
 from .builder import  QUANLAYERS, MTRANSFORMERS, \
                       build_quanlayer, build_mtransformer
 from .mTransformerv1 import mTransformerV1
+from .mTransformerv2 import mTransformerV2
                       
 __all__=['QUANLAYERS', 'MTRANSFORMERS', \
          'build_quanlayer', 'build_mtransformer']
